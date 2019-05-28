4c7526df8b05bd19e3d03c8fefc96ebdddbf8f19
curroDaniele <curro.daniele@iisgalvanimi.edu.it>
2019-05-28 10:23:48 +0000

    primo commit

---
+++ macchine/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DriveComponent } from './drive/drive.component';
+import { HistoryComponent } from './history/history.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DriveComponent,
+    HistoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
